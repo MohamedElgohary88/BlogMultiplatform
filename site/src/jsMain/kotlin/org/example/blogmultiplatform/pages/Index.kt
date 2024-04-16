ec14e9594e7e5f4605f369320fdb0da49f968c20
Mohamed Elgohary <mohamed.abdelazim.elgohary@gmail.com>
2024-04-16 18:37:10 +0200

    happy commit

---
+++ site/src/jsMain/kotlin/org/example/blogmultiplatform/pages/Index.kt
@@ -0,0 +1,25 @@
+package org.example.blogmultiplatform.pages
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.core.Page
+import org.jetbrains.compose.web.dom.Text
+import com.varabyte.kobweb.worker.rememberWorker
+import org.example.blogmultiplatform.worker.EchoWorker
+
+@Page
+@Composable
+fun HomePage() {
+    val worker = rememberWorker { EchoWorker { output -> console.log("Echoed: $output") } }
+    LaunchedEffect(Unit) {
+        worker.postInput("Hello, worker!")
+    }
+
+    // TODO: Replace the following with your own content
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("THIS PAGE INTENTIONALLY LEFT BLANK")
+    }
+}
